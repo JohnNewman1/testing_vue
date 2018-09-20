da5f34024b1d2a402d4be7f28857dcc21250c5c3
John Newman <newmanj903@gmail.com>
2018-09-20 12:37:01 +0100

    Testing Action promises

---
+++ test/api/README.md
@@ -0,0 +1,9 @@
+## Testing the api folder
+
+Generally we will not need to test the files in this folder as they are straight forward.
+
+Our Url files which store our url addresses will not need to be tested at all as they are files just saving data
+
+Our Index files can be tested to make sure they run correctly, however as axios is a trusted library this may be overkill
+
+Some methods in index files do hold some extra logic in that case they should be tested
---
+++ test/store/main/actions.test.js
@@ -0,0 +1,29 @@
+import action from '../../../src/store/modules/main/actions';
+import * as api from '../../../src/api/index';
+jest.mock('../../../src/api/index')
+
+
+describe('actions', () => {
+  let commit;
+  let state;
+  let resp;
+  console.error = jest.fn();
+
+  it('When promise resolves runs commit with correct arguments', async () => {
+    commit = jest.fn();
+    state = jest.fn();
+    resp = 'success'
+    api.getThemAll.mockImplementation(() => Promise.resolve(resp));
+    await action.CATCH_THEM_ALL({commit, state})
+    expect(commit).toBeCalledWith('STORE_THEM_ALL', 'success');
+  })
+
+  it('When promise rejects commit is not called', async () => {
+    commit = jest.fn();
+    state = jest.fn();
+    resp = new Error();
+    api.getThemAll.mockImplementation(() => Promise.reject(resp));
+    await action.CATCH_THEM_ALL({commit, state});
+    expect(commit).not.toHaveBeenCalled();
+  })
+})
